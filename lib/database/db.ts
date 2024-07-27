92454486ec5ce01ef33c95bf48999924520364c7
jkap86 <jkap86@gmail.com>
2024-07-27 16:33:23 -0400

    Update db.ts

--- lib/database/db.ts
+++ lib/database/db.ts
@@ -4,6 +4,7 @@ const pool = new Pool({
   connectionString:
     process.env.DATABASE_URL ||
     "postgres://postgres:password123@localhost:5432/postgres",
+  ssl: {rejectUnauthorized: false}
 });
 
 export default pool;
